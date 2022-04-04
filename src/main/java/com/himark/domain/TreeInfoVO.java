0642ef9365534b2c55bc1e6e512db03e8f56bce6
chanho470 <chanho470@naver.com>
2022-04-04 12:06:45 +0900

    직원추가(관리자) 누락 파일 업로드 TreeInfoVO

---
+++ src/main/java/com/himark/domain/TreeInfoVO.java
@@ -0,0 +1,14 @@
+package com.himark.domain;
+
+import lombok.Data;
+
+@Data 
+public class TreeInfoVO {
+	private String head_id;
+	private String head;
+	private String dept_id;
+	private String depart;
+	private String team_id;
+	private String team;	
+	
+}
