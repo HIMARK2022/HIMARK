f90fbb4de17b7bbea6cd2f65d17af92b1be0daab
chanho470 <chanho470@naver.com>
2022-04-07 08:15:41 +0900

    승인자 관리(관리자) 기능추가 : 승인자 지정시 승인타겟 연결.

--- src/main/java/com/himark/controller/AdminSelectApprover.java
+++ src/main/java/com/himark/controller/AdminSelectApprover.java
@@ -1,5 +1,8 @@
 package com.himark.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.himark.domain.ManagerVO;
 import com.himark.domain.UserVO;
 import com.himark.service.AdminAddEmployeeService;
 import com.himark.service.AdminSelectApproverService;
@@ -42,6 +46,25 @@ public class AdminSelectApprover {
 	@PostMapping("/update_authority")
 	public UserVO mod(Model model ,UserVO uservo) {
 		
+		String dept_id = uservo.getDept_id();
+		
+		String user_id = uservo.getUser_id();
+		
+		String manager_id = SelectApproverService.findOrgApprover(dept_id).get(0).getUser_id().toString();
+		//선택한 사용자가 속한 부서의 승인자(인사연동으로 들어온 원 승인자)
+		
+		
+		List<ManagerVO> targets = SelectApproverService.approvalTarget(manager_id);
+		
+		log.info(targets);
+		for(int i=0;i<targets.size();i++) {
+			
+			SelectApproverService.AddNewApprover(user_id, 
+					targets.get(i).getApproval_target().toString(), 
+					targets.get(i).getClassify_target().toString());
+			
+		}//승인타겟을 최초 승인자와 같이한다.
+		
 		SelectApproverService.updateAuthority("A2", uservo.getUser_id());
 		
 		return uservo;
@@ -52,6 +75,8 @@ public class AdminSelectApprover {
 	public UserVO del(Model model ,UserVO uservo) {
 		
 		SelectApproverService.updateAuthority("A1", uservo.getUser_id());
+		
+		SelectApproverService.deleteApprover(uservo.getUser_id());
 		
 		return uservo;
 	}
--- src/main/java/com/himark/service/AdminSelectApproverServiceImpl.java
+++ src/main/java/com/himark/service/AdminSelectApproverServiceImpl.java
@@ -1,8 +1,12 @@
 package com.himark.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.himark.domain.ManagerVO;
+import com.himark.domain.UserVO;
 import com.himark.mapper.AdminSelectApproverMapper;
 
 import lombok.Setter;
@@ -19,6 +23,34 @@ public class AdminSelectApproverServiceImpl implements AdminSelectApproverServic
 	public int updateAuthority(String authority_code, String user_id) {
 		
 		int cnt = mapper.updateAuthority(authority_code, user_id);
+		
+		return cnt;
+	}
+
+	@Override
+	public List<UserVO> findOrgApprover(String dept_id) {
+
+		return mapper.findOrgApprover(dept_id);
+	}
+
+	@Override
+	public List<ManagerVO> approvalTarget(String Manager_id) {
+		
+		return mapper.approvalTarget(Manager_id);
+	}
+
+	@Override
+	public int AddNewApprover(String manager_id, String approval_target, String classify_target) {
+		
+		int cnt =mapper.AddNewApprover(manager_id, approval_target, classify_target);
+		
+		return cnt;
+	}
+
+	@Override
+	public int deleteApprover(String manager_id) {
+		
+		int cnt = mapper.deleteApprover(manager_id);
 		
 		return cnt;
 	}
